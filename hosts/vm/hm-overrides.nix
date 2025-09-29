b45c6d4b87b0d545bbac8b167af9edabbfea10be
Roverp <yar.zubaha@proton.me>
2025-09-29 13:19:21 +0200

    chore(hyprland): small hyprland fixes

--- hosts/vm/hm-overrides.nix
+++ hosts/vm/hm-overrides.nix
@@ -1,7 +1,7 @@
 {...}: {
-  # hydenix.hm = {
-  #   hyprland.extraConfig = ''
-  #     monitor=Virtual-1,1920x1080,0x0,1
-  #   '';
-  # };
+  wayland.windowManager.hyprland = {
+    extraConfig = ''
+      monitor=Virtual-1,1920x1080,0x0,1
+    '';
+  };
 }
