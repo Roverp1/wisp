5c695e0cf475b79ddee523e7a85eaf2366588b5b
Roverp <yar.zubaha@proton.me>
2025-09-29 20:05:33 +0200

    feat(waybar): customize waybar for dwm like behaviour and look

--- modules/hm/wayland/waybar.nix
+++ modules/hm/wayland/waybar.nix
@@ -8,6 +8,38 @@ in {
   config = lib.mkIf cfg.enable {
     programs.waybar = {
       enable = true;
+      systemd.enable = true;
+
+      settings = {
+        mainBar = {
+          position = "top";
+          layer = "top";
+          height = 25;
+
+          modules-left = ["hyprland/workspaces" "hyprland/window"];
+          modules-center = [];
+          modules-right = ["network" "battery" "tray" "clock"];
+
+          "hyprland/window" = {
+            separate-outputs = true;
+          };
+
+          clock = {
+            format = "{:%b %e %I:%M %p}";
+          };
+
+          tray = {
+            icon-size = 18;
+            spacing = 10;
+          };
+
+          battery = {
+            format = "{capacity}%";
+          };
+        };
+      };
+
+      style = ./../../../Configs/.config/waybar/style.css;
     };
   };
 }
