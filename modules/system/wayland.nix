dd8c8bd0c71b3abda03c335c2c9d38b5b3c09fc1
Roverp <yar.zubaha@proton.me>
2025-09-29 12:37:33 +0200

    feat(system): create wayland module

---
+++ modules/system/wayland.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.wisp.wayland;
+in {
+  options.wisp.wayland = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      desription = "Enable wayland module";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.variables = {
+      NIXOS_OZONE_WL = "1";
+    };
+
+    programs.hyprland = {
+      package = pkgs.hyprland;
+      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+      enable = true;
+      withUWSM = false;
+    };
+  };
+}
