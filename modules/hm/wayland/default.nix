fdec453b7475f18686bbf7f412cadab331f41bcc
Roverp <yar.zubaha@proton.me>
2025-09-29 14:23:43 +0200

    fix: wayland system/hm modules

--- modules/hm/wayland/default.nix
+++ modules/hm/wayland/default.nix
@@ -9,10 +9,16 @@
   ];
 
   options.wisp.wayland = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable home-manager wayland module (system's wayland module can be enabled for better integration)";
+    };
+
     hyprland.enable = lib.mkOption {
       type = lib.types.bool;
       default = config.wisp.wayland.enable;
-      description = "Enable hyprland module";
+      description = "Enable home-manager hyprland module (system's hyprland module can be enabled for better integration)";
     };
 
     waybar.enable = lib.mkOption {
--- modules/system/wayland.nix
+++ modules/system/wayland.nix
@@ -12,6 +12,12 @@ in {
       default = true;
       description = "Enable wayland module";
     };
+
+    hyprland.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable hyprland module (home-manager integration needs to be enabled separately)";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -19,7 +25,7 @@ in {
       NIXOS_OZONE_WL = "1";
     };
 
-    programs.hyprland = {
+    programs.hyprland = lib.mkIf cfg.hyprland.enable {
       package = pkgs.hyprland;
       portalPackage = pkgs.xdg-desktop-portal-hyprland;
       enable = true;
