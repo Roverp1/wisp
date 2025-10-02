7c4515b6d3630b2a657391fe4f4c7e208e4e5891
Roverp <yar.zubaha@proton.me>
2025-10-02 20:10:11 +0200

    chore: fix syntax error

--- modules/hm/neovim.nix
+++ modules/hm/neovim.nix
@@ -10,7 +10,7 @@ in {
     enable = lib.mkOption {
       type = lib.types.bool;
       default = true;
-      desription = "Enable neovim module";
+      description = "Enable neovim module";
     };
   };
 
