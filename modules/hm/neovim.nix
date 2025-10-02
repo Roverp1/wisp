8ffed46dd2916d49dfef440b8dec2db86be8153d
Roverp <yar.zubaha@proton.me>
2025-10-02 14:12:57 +0200

    feat(neovim): make it a nix module

--- modules/hm/neovim.nix
+++ modules/hm/neovim.nix
@@ -1,84 +1,101 @@
-{pkgs, ...}: {
-  programs.neovim = let
-    toLua = str: "lua << EOF\n${str}\nEOF\n";
-    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.wisp.programs.neovim;
+in {
+  options.wisp.programs.neovim = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      desription = "Enable neovim module";
+    };
+  };
 
-    userPlugins = pkgs.userPkgs.vimPlugins;
-  in {
-    enable = true;
-    defaultEditor = true;
-    vimAlias = true;
+  config = lib.mkIf cfg.enable {
+    programs.neovim = let
+      toLua = str: "lua << EOF\n${str}\nEOF\n";
+      toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
 
-    extraPackages = with pkgs; [
-      pkgs.userPkgs.yazi
+      userPlugins = pkgs.userPkgs.vimPlugins;
+    in {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
 
-      gcc
-      nodejs
+      extraPackages = with pkgs;
+        [
+          gcc
+          nodejs
 
-      ripgrep
-      fd
+          ripgrep
+          fd
 
-      lua-language-server
-      nixd
+          lua-language-server
+          nixd
 
-      stylua
-      alejandra
-      prettierd
-      gotools
-    ];
+          stylua
+          alejandra
+          prettierd
+          gotools
+        ]
+        ++ lib.optional config.wisp.programs.yazi.enable pkgs.userPkgs.yazi;
 
-    plugins = with pkgs.vimPlugins; [
-      {
-        plugin = telescope-nvim;
-        config = toLua "require(\"telescope\").setup()";
-      }
+      plugins = with pkgs.vimPlugins; [
+        {
+          plugin = telescope-nvim;
+          config = toLua "require(\"telescope\").setup()";
+        }
 
-      {
-        plugin = nvim-lspconfig;
-        config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/lspconfig.lua;
-      }
+        {
+          plugin = nvim-lspconfig;
+          config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/lspconfig.lua;
+        }
 
-      {
-        plugin = userPlugins.blink-cmp;
-        config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/blink.lua;
-      }
+        {
+          plugin = userPlugins.blink-cmp;
+          config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/blink.lua;
+        }
 
-      {
-        plugin = luasnip;
-        config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/luasnip.lua;
-      }
+        {
+          plugin = luasnip;
+          config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/luasnip.lua;
+        }
 
-      {
-        plugin = conform-nvim;
-        config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/conform.lua;
-      }
+        {
+          plugin = conform-nvim;
+          config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/conform.lua;
+        }
 
-      {
-        plugin = yazi-nvim;
-        config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/yazi.lua;
-      }
+        {
+          plugin = yazi-nvim;
+          config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/yazi.lua;
+        }
 
-      vim-tmux-navigator
+        vim-tmux-navigator
 
-      telescope-fzf-native-nvim
+        telescope-fzf-native-nvim
 
-      nvim-web-devicons
-      plenary-nvim
+        nvim-web-devicons
+        plenary-nvim
 
-      {
-        plugin = nvim-treesitter.withPlugins (p: [
-          p.tree-sitter-nix
-          p.tree-sitter-lua
-        ]);
-        config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/treesitter.lua;
-      }
-    ];
+        {
+          plugin = nvim-treesitter.withPlugins (p: [
+            p.tree-sitter-nix
+            p.tree-sitter-lua
+          ]);
+          config = toLuaFile ./../../Configs/.config/nvim/lua/plugins/treesitter.lua;
+        }
+      ];
 
-    extraLuaConfig = ''
-      ${builtins.readFile ./../../Configs/.config/nvim/init.lua}
-      ${builtins.readFile ./../../Configs/.config/nvim/lua/config/keymaps.lua}
-      ${builtins.readFile ./../../Configs/.config/nvim/lua/config/options.lua}
-      ${builtins.readFile ./../../Configs/.config/nvim/lua/plugins/init.lua}
-    '';
+      extraLuaConfig = ''
+        ${builtins.readFile ./../../Configs/.config/nvim/init.lua}
+        ${builtins.readFile ./../../Configs/.config/nvim/lua/config/keymaps.lua}
+        ${builtins.readFile ./../../Configs/.config/nvim/lua/config/options.lua}
+        ${builtins.readFile ./../../Configs/.config/nvim/lua/plugins/init.lua}
+      '';
+    };
   };
 }
