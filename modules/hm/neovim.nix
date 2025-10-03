65e6a6b825ff78680b738dd2c91e7f3e2316fd54
Roverp <yar.zubaha@proton.me>
2025-10-03 13:30:55 +0200

    fix(neovim/lualine): syntax error

--- modules/hm/neovim.nix
+++ modules/hm/neovim.nix
@@ -64,21 +64,23 @@ in {
           {
             plugin = lualine-nvim;
             config = toLua ''
-              base00 = ${base00};
-              base01 = ${base01};
-              base02 = ${base02};
-              base03 = ${base03};
-              base04 = ${base04};
-              base05 = ${base05};
-              base06 = ${base06};
-              base07 = ${base07};
-              base08 = ${base08};
-              base09 = ${base09};
-              base0A = ${base0A};
-              base0B = ${base0B};
-              base0C = ${base0C};
-              base0D = ${base0D};
-              base0E = ${base0E};
+              colors = {
+                base00 = "${base00}",
+                base01 = "${base01}",
+                base02 = "${base02}",
+                base03 = "${base03}",
+                base04 = "${base04}",
+                base05 = "${base05}",
+                base06 = "${base06}",
+                base07 = "${base07}",
+                base08 = "${base08}",
+                base09 = "${base09}",
+                base0A = "${base0A}",
+                base0B = "${base0B}",
+                base0C = "${base0C}",
+                base0D = "${base0D}",
+                base0E = "${base0E}",
+              }
 
               ${builtins.readFile ./../../Configs/.config/nvim/lua/plugins/lualine.lua}
             '';
