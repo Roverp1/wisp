93493e0de04112be2bc8a098c053b201c275b3cf
Roverp <yar.zubaha@proton.me>
2025-10-04 12:55:38 +0200

    feat(zen-browser): switch to beta release

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
     };
 
     zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
+      url = "github:0xc000022070/zen-browser-flake/beta";
       # IMPORTANT: we're using "libgbm" and is only available in unstable so ensure
       # to have it up-to-date or simply don't specify the nixpkgs input
       inputs.nixpkgs.follows = "nixpkgs";
