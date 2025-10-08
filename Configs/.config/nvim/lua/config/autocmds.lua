a7bb26ae4408e5c32c041878840131b1e1eac04f
Roverp <yar.zubaha@proton.me>
2025-10-08 02:25:57 +0200

    fix(neovim): -> tabstop autocmd logic

--- Configs/.config/nvim/lua/config/autocmds.lua
+++ Configs/.config/nvim/lua/config/autocmds.lua
@@ -14,11 +14,12 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = {
 		"lua",
 		"go",
+		"hyprlang",
 	},
 	callback = function()
-		vim.bo.tabstop = 2
-		vim.bo.shiftwidth = 2
-		vim.bo.softtabstop = 2
+		vim.bo.tabstop = 4
+		vim.bo.shiftwidth = 4
+		vim.bo.softtabstop = 4
 	end,
 })
 
