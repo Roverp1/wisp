3218f30f1859430a631101485c057e187a7b251c
Roverp <yar.zubaha@proton.me>
2025-12-10 14:24:16 +0100

    feat(xdg): set zen-browser as default browser

--- modules/hm/xdg.nix
+++ modules/hm/xdg.nix
@@ -40,7 +40,16 @@ in {
         ];
       };
 
-      mimeApps.enable = true;
+      mimeApps = {
+        enable = true;
+
+        defaultApplications = {
+          "text/html" = "zen.desktop";
+          "x-scheme-handler/http" = "zen-beta.desktop";
+          "x-scheme-handler/https" = "zen-beta.desktop";
+          "x-scheme-handler/unknown" = "zen-beta.desktop";
+        };
+      };
 
       userDirs = {
         enable = true;
