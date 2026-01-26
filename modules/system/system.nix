07bb1803babbd2f4cc6e212299fdeeeb8fa536e2
Roverp <yar.zubaha@proton.me>
2026-01-26 18:01:56 +0100

    fix(stylix): error: 'noto-fonts-emoji' has been renamed to/replaced by 'noto-fonts-color-emoji'

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -19,7 +19,7 @@ in {
     fonts.packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
 
       nerd-fonts.jetbrains-mono
     ];
