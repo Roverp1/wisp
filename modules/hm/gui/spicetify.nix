27ebfc447b569ae3e1ef80baca8f30391aef0a93
Roverp <yar.zubaha@proton.me>
2026-01-27 19:06:34 +0100

    feat(spicetify): create custom theme based on text theme with dynamic base16 colors

--- modules/hm/gui/spicetify.nix
+++ modules/hm/gui/spicetify.nix
@@ -20,13 +20,49 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    stylix.targets.spicetify = {
+      enable = false;
+    };
+
     programs.spicetify = {
       enable = true;
 
+      # theme = spicePkgs.themes.text;
+      theme = {
+        name = "text";
+        src = ../../../Configs/.config/spicetify/themes/text;
+
+        # additonalCss = ''
+        #   .player-controls__buttons,
+        #   .main-nowPlayingBar-extraControls {
+        #     opacity: 1;
+        #   }
+        # '';
+      };
+
+      colorScheme = "custom";
+      customColorScheme = with config.lib.stylix.colors; {
+        accent = base0B;
+        accent-active = base0B;
+        accent-inactive = base00;
+        banner = base0B;
+        border-active = base0B;
+        border-inactive = base02;
+        header = base03;
+        highlight = base01;
+        main = base00;
+        notification = base0D;
+        notification-error = base08;
+        subtext = base04;
+        text = base05;
+      };
+
+      alwaysEnableDevTools = true;
+
       enabledExtensions = with spicePkgs.extensions; [
         adblock
-        beautifulLyrics
         keyboardShortcut
+        beautifulLyrics
         # betterGenres
         # oneko
       ];
